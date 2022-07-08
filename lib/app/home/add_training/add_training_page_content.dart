ea6debc2889c526f3463b824706410591e77af3f
Szymon Matecki <szymon.matecki2@gmail.com>
2022-07-08 12:51:05 +0200

    Implement adding workouts

--- lib/app/home/add_training/add_training_page_content.dart
+++ lib/app/home/add_training/add_training_page_content.dart
@@ -1,14 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddTrainingPageContent extends StatelessWidget {
+class AddTrainingPageContent extends StatefulWidget {
   const AddTrainingPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddTrainingPageContent> createState() => _AddTrainingPageContentState();
+}
+
+class _AddTrainingPageContentState extends State<AddTrainingPageContent> {
+  var workoutsName = '';
+  var trainingName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(
+          decoration: const InputDecoration(
+            hintText: 'Podaj nazwę treningu',
+          ),
+          onChanged: (newValue) {
+            setState(() {
+              workoutsName = newValue;
+            });
+          },
+        ),
+        TextField(
+          decoration: const InputDecoration(
+            hintText: 'Podaj nazwę ćwiczenia',
+          ),
+          onChanged: (newValue) {
+            setState(() {
+              trainingName = newValue;
+            });
+          },
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            FirebaseFirestore.instance.collection('workouts').add({
+              'name': workoutsName,
+              'training': trainingName,
+              'repeat': 8,
+              'series': 4,
+            });
+          },
+          child: const Text('Dodaj'),
+        ),
+      ],
     );
   }
 }
