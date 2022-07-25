f1c7d386e22f0494149b197640a8b502131f1280
Szymon Matecki <szymon.matecki2@gmail.com>
2022-07-25 20:09:56 +0200

    Delete to import

--- lib/app/features/home/my_account/my_account_page_content.dart
+++ lib/app/features/home/my_account/my_account_page_content.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kalistenika_2/app/cubit/root_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
